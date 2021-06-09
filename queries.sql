7757d88d3b4e0ae63492448d39ae4c3f8da58996
wanyonyi <wanyonyi@techmata.co.ke>
2021-06-09 21:59:52 +0300

    Creates basic CRUD using sqlc

--- queries.sql
+++ queries.sql
@@ -1,43 +1,68 @@
---name: GetUser :one
+-- name: GetUser :one
 SELECT * FROM users
 WHERE id = $1 LIMIT 1;
 
---name: ListUsers :many
+-- name: ListUsers :many
 SELECT * FROM users
 ORDER BY createdAt;
 
---name: CreateUser :one
+-- name: CreateUser :one
 INSERT INTO users (
-    email,username, profile_pic, status
-) VALUES(
-    $1, $2, $3, $4
+  email,username, profile_pic, status
+) VALUES (
+  $1, $2, $3, $4
 )
 RETURNING *;
 
---name GetInterest :one
+
+-- name: GetInterest :one
 SELECT * FROM interests
 WHERE id = $1 LIMIT 1;
 
---name: ListInterests :many
+-- name: ListInterests :many
 SELECT * FROM interests
 ORDER BY id;
 
---name: CreateInterest :one
+-- name: CreateInterest :one
 INSERT INTO interests (
     interest_name, interest_img
 ) VALUES(
     $1, $2
-)
-RETURNING *;
+) RETURNING *;
 
---name: CreatePlace :one
+-- name: CreatePlace :one
 INSERT INTO places (
     place_name, location,location_name,palce_img,interest_id
 ) VALUES (
     $1, $2, $3, $4, $5
-)
-RETURNING *;
+) RETURNING *;
 
---name GetPlaces :many
+-- name ListPlaces :many
 SELECT * FROM  places
 ORDER BY id;
+
+-- name: CreateTrip :one
+INSERT INTO trips (
+    trip_name, duration, cost, start_date, status, orgernizer
+) VALUES (
+    $1, $2, $3, $4, $5, $6
+) RETURNING *;
+
+-- name: GetTrip :one
+SELECT * FROM trips
+WHERE id = $1 LIMIT 1;
+
+-- name: ListTrips :many
+SELECT * FROM trips
+ORDER BY id;
+
+-- name: CreateTripMember :one
+INSERT INTO trip_members (
+    trip_id, member
+) VALUES (
+    $1, $2
+) RETURNING *;
+
+-- name: ListTripMembers :many
+SELECT * FROM trip_members 
+WHERE trip_id = $1;
--- schema.sql
+++ schema.sql
@@ -1,44 +1,43 @@
-CREATE TYPE status AS ENUM (
-  'active',
-  'inactive'
-);
+-- Example queries for sqlc
 CREATE TABLE users (
-    id BIGSERIAL PRIMARY KEY,
-    email text NOT NULL,
-    username text NOT NULL,
-    profile_pic text,
-    status status  NOT NULL,
-    createdAt timestamp NOT NULL DEFAULT NOW(),
-    updatedAt timestamp  
+  id BIGSERIAL PRIMARY KEY,
+  email text      NOT NULL,
+  username  text NOT NULL,
+  status int NOT NULL,
+  profile_pic text,
+  is_active int  NOT NULL,
+  createdAt timestamp NOT NULL DEFAULT NOW(),
+  updatedAt timestamp 
 );
 
 CREATE TABLE interests (
-    id SERIAL PRIMARY KEY,
-    interest_name text NOT NULL,
-    interest_img text NOT NULL,
-    updatedAt timestamp  
+  id SERIAL PRIMARY KEY,
+  interest_name text NOT NULL,
+  interest_img text NOT NULL,
+  updatedAt timestamp  
 );
 
 CREATE TABLE places (
-    id SERIAL PRIMARY KEY,
-    place_name text NOT NULL,
-    location text NOT NULL,
-    location_name text NOT NULL,
-    palce_img text NOT NULL,
-    interest_id SERIAL NOT NULL REFERENCES interests(id)
+  id SERIAL PRIMARY KEY,
+  place_name text NOT NULL,
+  location text NOT NULL,
+  location_name text NOT NULL,
+  palce_img text NOT NULL,
+  interest_id SERIAL NOT NULL REFERENCES interests(id)
 );
 
 CREATE TABLE trips(
-    id SERIAL PRIMARY KEY,
-    trip_name text NOT NULL,
-    cost int NOT NULL,
-    start_date timestamp NOT NULL,
-    status status  NOT NULL,
-    orgernizer SERIAL NOT NULL REFERENCES users(id)
+  id SERIAL PRIMARY KEY,
+  trip_name text NOT NULL,
+  cost int NOT NULL,
+  duration int NOT NULL,
+  start_date timestamp NOT NULL,
+  status int  NOT NULL,
+  orgernizer SERIAL NOT NULL REFERENCES users(id)
 );
 
 CREATE  TABLE trip_members(
-    id SERIAL PRIMARY KEY,
-    trip_id SERIAL NOT NULL REFERENCES trips(id),
-    member SERIAL NOT NULL REFERENCES users(id)
+  id SERIAL PRIMARY KEY,
+  trip_id SERIAL NOT NULL REFERENCES trips(id),
+  member SERIAL NOT NULL REFERENCES users(id)
 );
